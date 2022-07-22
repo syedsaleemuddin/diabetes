ad748563c9901ac326090befd8c974534d774382
syedsaleemuddin <105686450+syedsaleemuddin@users.noreply.github.com>
2022-07-22 16:49:59 +0530

    Update settings.py

--- DiabetesPrediction/DiabetesPrediction/settings.py
+++ DiabetesPrediction/DiabetesPrediction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2l0oc$bi$(zl4pgr27s$x=wgonv05v*7%!itmk(#fj2gkbn8-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'diaaaaa.herokuapp.com']
 
 
 # Application definition
